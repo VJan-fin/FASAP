7b47f4f4847f2db97fb065b7d05b8252b8381a08
Angela Josifovska <josifovska.angela@students.finki.ukim.mk>
2014-06-23 00:09:53 +0200

    eve

--- FASAP - (Food As Soon As Possible)/FASAP/Program.cs
+++ FASAP - (Food As Soon As Possible)/FASAP/Program.cs
@@ -16,7 +16,7 @@ namespace SmetkaZaNaracka
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             // plink odnadvor@bazi.finki.ukim.mk -ssh -pw B2eN66Rq3GXE7T8R -C -N -2 -L 1620:bazi.finki.ukim.mk:1521
-            // Application.Run(new FasapPocetenEkran());
+            Application.Run(new FasapPocetenEkran());
           
             //Application.Run(new PregledMeni());
 
@@ -24,7 +24,7 @@ namespace SmetkaZaNaracka
             //Application.Run(new FasapPoceten());
 
 
-            Application.Run(new PregledKvartalnaSostojba());
+            //Application.Run(new PregledKvartalnaSostojba());
             //Application.Run(new Izv1PridonesVoPromet());
 
         }
