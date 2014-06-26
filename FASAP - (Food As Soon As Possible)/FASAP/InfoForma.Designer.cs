eecc3a35e12260abe609b507d846a3c9aa2bb6a7
VJan-fin <vik>
2014-06-26 20:53:56 +0200

    Mali modifikacii vo info formata

--- FASAP - (Food As Soon As Possible)/FASAP/InfoForma.Designer.cs
+++ FASAP - (Food As Soon As Possible)/FASAP/InfoForma.Designer.cs
@@ -33,8 +33,14 @@
             this.label1 = new System.Windows.Forms.Label();
             this.pictureBox3 = new System.Windows.Forms.PictureBox();
             this.labelFASAP1 = new SmetkaZaNaracka.LabelFASAP(this.components);
+            this.labelFASAP4 = new SmetkaZaNaracka.LabelFASAP(this.components);
             this.labelFASAP2 = new SmetkaZaNaracka.LabelFASAP(this.components);
             this.labelFASAP3 = new SmetkaZaNaracka.LabelFASAP(this.components);
+            this.labelFASAP5 = new SmetkaZaNaracka.LabelFASAP(this.components);
+            this.labelFASAP6 = new SmetkaZaNaracka.LabelFASAP(this.components);
+            this.labelFASAP7 = new SmetkaZaNaracka.LabelFASAP(this.components);
+            this.labelFASAP8 = new SmetkaZaNaracka.LabelFASAP(this.components);
+            this.labelFASAP9 = new SmetkaZaNaracka.LabelFASAP(this.components);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
             this.SuspendLayout();
             // 
@@ -65,11 +71,25 @@
             this.labelFASAP1.ForeColor = System.Drawing.Color.Khaki;
             this.labelFASAP1.Image = ((System.Drawing.Image)(resources.GetObject("labelFASAP1.Image")));
             this.labelFASAP1.LblObject = null;
-            this.labelFASAP1.Location = new System.Drawing.Point(487, 122);
+            this.labelFASAP1.Location = new System.Drawing.Point(456, 64);
             this.labelFASAP1.Name = "labelFASAP1";
-            this.labelFASAP1.Size = new System.Drawing.Size(512, 195);
+            this.labelFASAP1.Size = new System.Drawing.Size(427, 142);
             this.labelFASAP1.TabIndex = 10;
-            this.labelFASAP1.Text = resources.GetString("labelFASAP1.Text");
+            this.labelFASAP1.Text = "FASAP е апликација чија примарна цел е да претставува посредник помеѓу ресторанит" +
+                "е и нивните клиенти";
+            // 
+            // labelFASAP4
+            // 
+            this.labelFASAP4.BackColor = System.Drawing.Color.Transparent;
+            this.labelFASAP4.Font = new System.Drawing.Font("Trebuchet MS", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelFASAP4.ForeColor = System.Drawing.Color.Khaki;
+            this.labelFASAP4.Image = ((System.Drawing.Image)(resources.GetObject("labelFASAP4.Image")));
+            this.labelFASAP4.LblObject = null;
+            this.labelFASAP4.Location = new System.Drawing.Point(934, 121);
+            this.labelFASAP4.Name = "labelFASAP4";
+            this.labelFASAP4.Size = new System.Drawing.Size(290, 142);
+            this.labelFASAP4.TabIndex = 13;
+            this.labelFASAP4.Text = "На корисниците им се нуди преглед на рестораните според различни критериуми";
             // 
             // labelFASAP2
             // 
@@ -78,11 +98,11 @@
             this.labelFASAP2.ForeColor = System.Drawing.Color.Khaki;
             this.labelFASAP2.Image = ((System.Drawing.Image)(resources.GetObject("labelFASAP2.Image")));
             this.labelFASAP2.LblObject = null;
-            this.labelFASAP2.Location = new System.Drawing.Point(96, 386);
+            this.labelFASAP2.Location = new System.Drawing.Point(526, 240);
             this.labelFASAP2.Name = "labelFASAP2";
-            this.labelFASAP2.Size = new System.Drawing.Size(903, 154);
-            this.labelFASAP2.TabIndex = 11;
-            this.labelFASAP2.Text = resources.GetString("labelFASAP2.Text");
+            this.labelFASAP2.Size = new System.Drawing.Size(282, 142);
+            this.labelFASAP2.TabIndex = 14;
+            this.labelFASAP2.Text = "Со избор на ресторан може да се погледне и неговата целокупна понуда";
             // 
             // labelFASAP3
             // 
@@ -91,19 +111,92 @@
             this.labelFASAP3.ForeColor = System.Drawing.Color.Khaki;
             this.labelFASAP3.Image = ((System.Drawing.Image)(resources.GetObject("labelFASAP3.Image")));
             this.labelFASAP3.LblObject = null;
-            this.labelFASAP3.Location = new System.Drawing.Point(101, 591);
+            this.labelFASAP3.Location = new System.Drawing.Point(602, 420);
             this.labelFASAP3.Name = "labelFASAP3";
-            this.labelFASAP3.Size = new System.Drawing.Size(898, 131);
-            this.labelFASAP3.TabIndex = 12;
-            this.labelFASAP3.Text = resources.GetString("labelFASAP3.Text");
+            this.labelFASAP3.Size = new System.Drawing.Size(269, 142);
+            this.labelFASAP3.TabIndex = 15;
+            this.labelFASAP3.Text = "Изберете некои од вкусните производи и направете ја својата прва нарачка";
+            // 
+            // labelFASAP5
+            // 
+            this.labelFASAP5.BackColor = System.Drawing.Color.Transparent;
+            this.labelFASAP5.Font = new System.Drawing.Font("Trebuchet MS", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelFASAP5.ForeColor = System.Drawing.Color.Khaki;
+            this.labelFASAP5.Image = ((System.Drawing.Image)(resources.GetObject("labelFASAP5.Image")));
+            this.labelFASAP5.LblObject = null;
+            this.labelFASAP5.Location = new System.Drawing.Point(91, 367);
+            this.labelFASAP5.Name = "labelFASAP5";
+            this.labelFASAP5.Size = new System.Drawing.Size(368, 142);
+            this.labelFASAP5.TabIndex = 16;
+            this.labelFASAP5.Text = "FASAP  e одлична алатка за управување со финансиите и следење на работата на рест" +
+                "ораните";
+            // 
+            // labelFASAP6
+            // 
+            this.labelFASAP6.BackColor = System.Drawing.Color.Transparent;
+            this.labelFASAP6.Font = new System.Drawing.Font("Trebuchet MS", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelFASAP6.ForeColor = System.Drawing.Color.Khaki;
+            this.labelFASAP6.Image = ((System.Drawing.Image)(resources.GetObject("labelFASAP6.Image")));
+            this.labelFASAP6.LblObject = null;
+            this.labelFASAP6.Location = new System.Drawing.Point(59, 545);
+            this.labelFASAP6.Name = "labelFASAP6";
+            this.labelFASAP6.Size = new System.Drawing.Size(285, 142);
+            this.labelFASAP6.TabIndex = 17;
+            this.labelFASAP6.Text = "Менаџерите имаат брз пристап до сите податоци за ресторанот и неговите вработени";
+            // 
+            // labelFASAP7
+            // 
+            this.labelFASAP7.BackColor = System.Drawing.Color.Transparent;
+            this.labelFASAP7.Font = new System.Drawing.Font("Trebuchet MS", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelFASAP7.ForeColor = System.Drawing.Color.Khaki;
+            this.labelFASAP7.Image = ((System.Drawing.Image)(resources.GetObject("labelFASAP7.Image")));
+            this.labelFASAP7.LblObject = null;
+            this.labelFASAP7.Location = new System.Drawing.Point(384, 602);
+            this.labelFASAP7.Name = "labelFASAP7";
+            this.labelFASAP7.Size = new System.Drawing.Size(299, 142);
+            this.labelFASAP7.TabIndex = 18;
+            this.labelFASAP7.Text = "За да го регистрирате Вашиот ресторан притиснете на \"Регистрирај нов ресторан\"";
+            // 
+            // labelFASAP8
+            // 
+            this.labelFASAP8.BackColor = System.Drawing.Color.Transparent;
+            this.labelFASAP8.Font = new System.Drawing.Font("Trebuchet MS", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelFASAP8.ForeColor = System.Drawing.Color.Khaki;
+            this.labelFASAP8.Image = ((System.Drawing.Image)(resources.GetObject("labelFASAP8.Image")));
+            this.labelFASAP8.LblObject = null;
+            this.labelFASAP8.Location = new System.Drawing.Point(949, 340);
+            this.labelFASAP8.Name = "labelFASAP8";
+            this.labelFASAP8.Size = new System.Drawing.Size(363, 170);
+            this.labelFASAP8.TabIndex = 19;
+            this.labelFASAP8.Text = "По најавата со своите корисничко име и лозинка, вработените добиваат преглед на р" +
+                "аботата која треба да ја извршат";
+            // 
+            // labelFASAP9
+            // 
+            this.labelFASAP9.BackColor = System.Drawing.Color.Transparent;
+            this.labelFASAP9.Font = new System.Drawing.Font("Trebuchet MS", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelFASAP9.ForeColor = System.Drawing.Color.Khaki;
+            this.labelFASAP9.Image = ((System.Drawing.Image)(resources.GetObject("labelFASAP9.Image")));
+            this.labelFASAP9.LblObject = null;
+            this.labelFASAP9.Location = new System.Drawing.Point(879, 602);
+            this.labelFASAP9.Name = "labelFASAP9";
+            this.labelFASAP9.Size = new System.Drawing.Size(290, 115);
+            this.labelFASAP9.TabIndex = 20;
+            this.labelFASAP9.Text = "За да се најавите на системот, притиснете \"Логирај се\"";
             // 
             // InfoForma
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1366, 768);
+            this.Controls.Add(this.labelFASAP9);
+            this.Controls.Add(this.labelFASAP8);
+            this.Controls.Add(this.labelFASAP7);
+            this.Controls.Add(this.labelFASAP6);
+            this.Controls.Add(this.labelFASAP5);
             this.Controls.Add(this.labelFASAP3);
             this.Controls.Add(this.labelFASAP2);
+            this.Controls.Add(this.labelFASAP4);
             this.Controls.Add(this.labelFASAP1);
             this.Controls.Add(this.pictureBox3);
             this.Controls.Add(this.label1);
@@ -115,8 +208,14 @@
             this.Controls.SetChildIndex(this.label1, 0);
             this.Controls.SetChildIndex(this.pictureBox3, 0);
             this.Controls.SetChildIndex(this.labelFASAP1, 0);
+            this.Controls.SetChildIndex(this.labelFASAP4, 0);
             this.Controls.SetChildIndex(this.labelFASAP2, 0);
             this.Controls.SetChildIndex(this.labelFASAP3, 0);
+            this.Controls.SetChildIndex(this.labelFASAP5, 0);
+            this.Controls.SetChildIndex(this.labelFASAP6, 0);
+            this.Controls.SetChildIndex(this.labelFASAP7, 0);
+            this.Controls.SetChildIndex(this.labelFASAP8, 0);
+            this.Controls.SetChildIndex(this.labelFASAP9, 0);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -128,7 +227,13 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.PictureBox pictureBox3;
         private LabelFASAP labelFASAP1;
+        private LabelFASAP labelFASAP4;
         private LabelFASAP labelFASAP2;
         private LabelFASAP labelFASAP3;
+        private LabelFASAP labelFASAP5;
+        private LabelFASAP labelFASAP6;
+        private LabelFASAP labelFASAP7;
+        private LabelFASAP labelFASAP8;
+        private LabelFASAP labelFASAP9;
     }
 }
